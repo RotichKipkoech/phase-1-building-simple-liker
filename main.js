fa045c2e105c4b4c403eec2b0636e1aea632ff8c
RotichKipkoech <kipkoechrottich@gmail.com>
2023-06-29 15:27:18 +0300

    Completed assingment

--- main.js
+++ main.js
@@ -4,7 +4,39 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Hide the error modal on page load
+const errorModal = document.querySelector('#modal');
+errorModal.classList.add('hidden');
 
+// Function to toggle the heart state
+function toggleHeartState(heart) {
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch((error) => {
+        errorModal.classList.remove('hidden');
+        const errorMessage = document.querySelector('#modal-message');
+        errorMessage.textContent = error;
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  } else {
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
+
+// Add click event listeners to all hearts
+const hearts = document.querySelectorAll('.like-glyph');
+hearts.forEach((heart) => {
+  heart.addEventListener('click', () => {
+    toggleHeartState(heart);
+  });
+});
 
 
 //------------------------------------------------------------------------------
